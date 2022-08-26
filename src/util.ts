29fe794edcc8a471df95946e4406bdbe461d081d
gemsvido <gemsvido@gmail.com>
2022-08-26 15:41:54 -0700

    add debug stream

--- src/util.ts
+++ src/util.ts
@@ -26,13 +26,16 @@ export default class Nil {
 	public static async getFile () : Promise<string> {
 		try {
 			if (Nil.latestVanillaFile === null) {
+				console.log("getFile - fetching new main.js");
 				const mainjs = await (await fetch(`https://www.deltamath.com/app/main.761406757919c0973f71.js`)).text();
 				Nil.latestVanillaFile = mainjs;
 				return mainjs;
 			} else {
+				console.log("getFile - returned latestVanilaFile");
 				return Nil.latestVanillaFile;
 			}
 		} catch (error : unknown) {
+			console.error("getFile - encountered error");
 			throw new Error(`Could not fetch main.js file.\nReason: ${error}`);
 		}
 	};
@@ -40,12 +43,13 @@ export default class Nil {
 	/** Applies patches to (unmodifiedFile). */
 	public static patchFile (unmodifiedFile : string) : string {
 
+		console.log("patchFile - patching main.js")
+
 		return `
-	
 		${Nil.es6`${unmodifiedFile}`}
 	
 		${Nil.es6`
-	
+
 		console.log("%cNil", "font-size:69px;color:#540052;font-weight:900;font-family:sans-serif;");
 		console.log("%cVersion ${VERSION}", "font-size:20px;color:#000025;font-weight:700;font-family:sans-serif;");
 		
@@ -68,8 +72,10 @@ export default class Nil {
 	/** Gets the latest patched main.js file. If it isn't cached, patch it now. */
 	public static async getPatchedFile () : Promise<string> {
 		if (Nil.latestPatchedFile !== null) {
+			console.log("getPatchedFile - returned lastPatchedFile");
 			return Nil.latestPatchedFile;
 		} else {
+			console.log("getPatchedFile - patching main.js now...");
 			const patchedfile = await (await Nil.patchFile(Nil.latestVanillaFile || (await (Nil.getFile())).valueOf())).valueOf();
 			Nil.latestPatchedFile = patchedfile;
 			return patchedfile;
