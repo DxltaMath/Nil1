19980de7569d1c0f7e1287fd8f06e5dd46148142
gemsvido <gemsvido@gmail.com>
2022-08-30 10:39:57 -0700

    Allow escaping timed problems!

--- src/util.ts
+++ src/util.ts
@@ -94,7 +94,28 @@ export default class Nil {
                     if ("__ngUnwrap__" === e) return t;
                     !1 === t(e) && (e.preventDefault(), e.returnValue = !1)
                 }
-            }`.replaceAll("\n", ""));
+            }
+			`.replaceAll("\n", ""));
+
+
+			// Allow escaping timed activities
+			patched = patched.replaceAll(`{if($(".timed-start-button").length&&"Stop"==$(".timed-start-button").text())return alertDialog("You must stop the timer before pressing back. ");this.router.url.startsWith("/explore")?this.router.navigate(["/explore"]):this.router.url.startsWith("/student")?this.router.navigate(["/student"]):this.location.back()}`, `
+			{
+				// Only happens while timer is running
+
+				/** Allow exiting timed problems without clicking "Stop" */
+				const escape = window.delta.allowEscapingTimed;
+
+				/** If the button says "Stop" */
+				const stop = $(".timed-start-button").length && "Stop" == $(".timed-start-button").text();
+
+				// If escape is false and stop is true, then do this:
+				if (!escape && stop) return alertDialog("You must stop the timer before pressing back. ");
+
+				// Otherwise do this:
+				this.router.url.startsWith("/explore") ? this.router.navigate(["/explore"]) : this.router.url.startsWith("/student") ? this.router.navigate(["/student"]) : this.location.back()
+			}
+			`.replaceAll("\n", ""));
 
 
 			const output : string = `
